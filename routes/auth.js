b4d46620c4ddef82f1ed82fb93318781819091ae
aitaj <aytajez@code.edu.az>
2024-03-11 16:52:38 +0400

    test

---
+++ routes/auth.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const sendMail = require('../services/mailService');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const MailTemplate = require('../templates/mailTemplate');
+const router = express.Router();
+const User = require('../models/user');
+
+const userValidationRules = require('../validations/userValidation');
+const validateUser = require('../middleware/validateUser');
+
+require('dotenv').config();
+
+router.post('/register', userValidationRules(), validateUser, async (req, res) => {
+    try {
+        const { username, password, firstname, lastname, email } = req.body;
+        const user = new User({ username, password, firstname, lastname, email });
+        await user.save();
+
+        const template = new MailTemplate(firstname, lastname, password);
+        sendMail(email, 'Hoşgeldiniz', null, template.getTemplate('register'));
+
+        res.status(201).send({
+            status: 'succeeded',
+            message: 'User Created Successfully',
+            content: user,
+            statusCode: 201
+        });
+    }
+    catch (err) {
+        res.status(400).send({
+            status: 'failed',
+            message: err.message,
+            content: err,
+            statusCode: 400
+        });
+    }
+});
+
+
+router.post('/login', async (req, res) => {
+
+    try {
+        
+        const { username, password } = req.body; 
+        const user = await User.findOne({ username });
+
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).send({
+                status: 'failed',
+                statusCode: 401,
+                message: 'Invalid Username Or Password',
+                token: ''
+            });
+        }
+
+        const tokenOptions = {
+            expiresIn: '20d', // s saniye, m dakika, h saat, d gün
+            // notBefore: '15s', // 15 saniye sonra başlar
+            // audience: 'http://localhost:3000', // token hangi sunucuda kullanılacak
+            // issuer: 'http://localhost:5001', // token kimin tarafından veriliyor
+            // jwtid: 'CodeAcademyTokenId', // token id
+            // subject: 'nodejs dersleri için üretilen token, pervinden icaze almadan servisler çalışmaz', // token hangi konuda
+            // algorithm: 'HS256' // token hangi algoritmayla şifrelenecek 
+        }
+
+        const token = jwt.sign({
+            userId: user._id  //,
+            // role: 'admin',
+            // manager: 'pervin',
+            // movzu: 'node.js',
+            // date: 'eski date',
+            // group:'RADFE203' 
+        }, process.env.SECRET_KEY, tokenOptions);
+        res.status(200).send({
+            status: 'succeeded',
+            statusCode: 200,
+            message: 'Login Success',
+            token: token
+        });
+    }
+    catch (err) {
+        res.status(400).send({
+            status: 'failed',
+            statusCode: 400,
+            message: err.message,
+            token: 'Invalid token'
+        });
+    }
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const cors = require('cors');
+const { Server } = require('socket.io');
+const http = require('http');
+
+require('dotenv').config();
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+    autoConnect: true,
+    cors: {
+        origin: '*'
+    }
+});
+
+
+io.on('connection', (socket) => {
+    console.log('Bir kullanıcı bağlandı.');
+
+    socket.on('addCategory', (newCategory) => {
+        console.log('Yeni kategori eklendi:', newCategory);
+        io.emit('categoryAdded', newCategory);
+    });
+
+
+
+
+    socket.on('notification', (notification) => {
+        console.log('Yeni bildirim:', notification); // db ye kaydedilebilir + mail gönderin, sms gönderin, push notification gönderin
+        io.emit('notification', notification);
+    });
+
+
+    socket.on('disconnect', () => {
+        console.log('Bir kullanıcı bağlantıyı kesti.');
+    });
+});
+
+
+app.use(express.json());
+app.use(cors())
+mongoose
+    .connect(process.env.DATABASE_SERVER_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to the database!');
+    }).catch((err) => {
+        console.log('Connection failed!', err);
+    });
+
+const authRouter = require('./routes/auth');
+const roleRouter = require('./routes/roles');
+const uploadRouter = require('./routes/uploads');
+const productsRouter = require('./routes/products');
+const userRoleRouter = require('./routes/userRoles');
+const categoriesRouter = require('./routes/categories');
+const permissionRouter = require('./routes/permissions');
+
+app.use('/api/auth', authRouter);
+app.use('/api/roles', roleRouter);
+app.use('/api/uploads', uploadRouter);
+app.use('/api/products', productsRouter);
+app.use('/api/userRoles', userRoleRouter);
+app.use('/api/categories', categoriesRouter);
+app.use('/api/permissions', permissionRouter);
+
+const SOCKET_PORT = process.env.SOCKET_PORT || 7001;
+io.listen(SOCKET_PORT, () => console.log(`socket.io server running on port ${SOCKET_PORT}`));
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
